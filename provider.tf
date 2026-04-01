9d1f8ca6c8afedf437412ed9a108b9ffc398b912
Gabriel Torres <gaabrieltt7@gmail.com>
2026-03-31 21:42:09 -0300

    feat: add vpc and backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "6.38.0"
     }
   }
+
+  backend "s3" {
+    bucket = "devops-lab-tfstate-gt"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+
 }
 
 provider "aws" {
