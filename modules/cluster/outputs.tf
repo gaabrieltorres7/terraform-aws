1be258c847a3311e662ebf71edaab66684e7c1cd
Gabriel Torres <gaabrieltt7@gmail.com>
2026-04-07 22:15:42 -0300

    feat: add outputs in cluster and network modules

--- modules/cluster/outputs.tf
+++ modules/cluster/outputs.tf
@@ -2,3 +2,18 @@ output "eks_cluster_name" {
   value       = aws_eks_cluster.eks_cluster.id
   description = "The name of the EKS cluster"
 }
+
+output "oidc" {
+  value       = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+  description = "The OIDC issuer URL for the EKS cluster"
+}
+
+output "certificate_authority" {
+  value       = aws_eks_cluster.eks_cluster.certificate_authority[0].data
+  description = "The certificate authority data for the EKS cluster"
+}
+
+output "endpoint" {
+  value       = aws_eks_cluster.eks_cluster.endpoint
+  description = "The endpoint for the EKS cluster"
+}
